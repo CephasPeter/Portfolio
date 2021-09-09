d7490fcd528a215cb2f1eab1d46f519bf0c5bf61
cephas <cephasarowolo@gmail.com>
2021-09-09 23:13:00 +0100

    Init

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,8 +11,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Cephas Arowolo",
+  title: "Hi all, I'm Cephas",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
